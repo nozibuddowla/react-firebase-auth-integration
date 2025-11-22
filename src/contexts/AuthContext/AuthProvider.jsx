b753d9e5df01d441e8f73103d22fe0c169ad5d77
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-11-22 19:03:30 +0600

    add sign in user using context api

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -1,17 +1,39 @@
 import React from "react";
 import { AuthContext } from "./AuthContext";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../../firebase/firebase.init";
 
 const AuthProvider = ({ children }) => {
+  // create account
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  // login
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // get current user info
+  onAuthStateChanged(auth, currentUser => {
+    if (currentUser) {
+      console.log("inside observer, if closure: ", currentUser);
+      
+    } else {
+      console.log("inside observer, else closure: ", currentUser);
+      
+    }
+  })
+
   const authInfo = {
     createUser,
+    signInUser,
   };
-  
+
   return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
 
